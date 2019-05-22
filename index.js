d205942a7fe374860c09bc745bed8a020e241bcb
Rob Ireton <rob.ireton@uc.edu>
2019-05-22 11:49:19 -0400

    tar and copy

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const path = require('path')
 const fs = require('fs-extra')
 const mysqldump = require('mysqldump')
+const tar = require('tar')
+const tmp = require('tmp')
 
 // provide a title to the process
 process.title = 'ghost-backup'
@@ -25,28 +27,40 @@ console.log(`using Ghost config at ${configPath}`)
 
 switch (config.database.client) {
   case 'sqlite3':
-  console.log(`sqlite3 database at ${config.database.connection.filename}`)
-  if (config.database.connection.filename.startsWith(instancePath)) {
-    console.log('database will be backed up at its live location')
-  }
-  else {
-    //Copy database file into instancePath for backup.
-    fs.ensureDirSync(dataPath)
-    const destination = path.resolve(dataPath, './ghost-backup.db')
-    fs.copySync(config.database.connection.filename, destination)
-    console.log(`Copied ${config.database.connection.filename} to ${destination} for backup`)
-  }
-  break
+    console.log(`sqlite3 database at ${config.database.connection.filename}`)
+    if (config.database.connection.filename.startsWith(instancePath)) {
+      console.log('database will be backed up at its live location')
+    } else {
+    // Copy database file into instancePath for backup.
+      fs.ensureDirSync(dataPath)
+      const destination = path.resolve(dataPath, 'ghost-backup.db')
+      fs.copySync(config.database.connection.filename, destination)
+      console.log(`Copied ${config.database.connection.filename} to ${destination} for backup`)
+    }
+    break
 
   case 'mysql':
-  console.log('mysql database…')
-  fs.ensureDirSync(dataPath)
-  const destination = path.resolve(dataPath, './ghost-backup.sql')
-  mysqldump({connection: config.database.connection, dumpToFile: destination})
-  console.log(`dumped ${config.database.connection.database}@${config.database.connection.host} to ${destination} for backup`)
-  break
+    console.log('mysql database…')
+    fs.ensureDirSync(dataPath)
+    const destination = path.resolve(dataPath, './ghost-backup.sql')
+    mysqldump({ connection: config.database.connection, dumpToFile: destination })
+    console.log(`dumped ${config.database.connection.database}@${config.database.connection.host} to ${destination} for backup`)
+    break
 
   default:
-  console.error(`I don’t know what to do with database client ${config.database.client}`)
-  process.exit(1)
+    console.error(`I don’t know what to do with database client ${config.database.client}`)
+    process.exit(1)
 }
+
+tmp.setGracefulCleanup()
+const tarball = tmp.tmpNameSync({ prefix: 'ghost-backup_', postfix: '.tgz' })
+console.log(`backing up ${path.basename(instancePath)} to ${tarball} from ${path.dirname(instancePath)}`)
+tar.c({
+  sync: true,
+  cwd: path.dirname(instancePath),
+  gzip: true,
+  file: tarball
+},
+[path.basename(instancePath)])
+
+fs.moveSync(tarball, path.resolve(argv.shift() || process.cwd(), 'ghost-backup.tgz'), { overwrite: true })
